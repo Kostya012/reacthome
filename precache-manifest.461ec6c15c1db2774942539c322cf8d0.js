f7331ceb935479badb1fa62d1d5e741d3e5d2c5c
Kostya <kostya120288@gmail.com>
2020-09-16 20:13:55 +0300

    Updates

---
+++ precache-manifest.461ec6c15c1db2774942539c322cf8d0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2002054ba2024c4cd9a9a9339ed408e",
+    "url": "/reacthome/index.html"
+  },
+  {
+    "revision": "5b8cc8076cc2d3481af9",
+    "url": "/reacthome/static/css/main.ebfbb6a7.chunk.css"
+  },
+  {
+    "revision": "a09abceef8d0c708491d",
+    "url": "/reacthome/static/js/2.845843c1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reacthome/static/js/2.845843c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b8cc8076cc2d3481af9",
+    "url": "/reacthome/static/js/main.9ffffb2a.chunk.js"
+  },
+  {
+    "revision": "6258ffb566b391c94579",
+    "url": "/reacthome/static/js/runtime-main.fd499c8a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reacthome/static/media/logo.5d5d9eef.svg"
+  }
+]);
